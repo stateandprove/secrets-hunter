0a06a29c4c28068ce7745870171fd52213408051
stateandprove <stateandprove050@gmail.com>
2026-02-21 20:44:02 +0100

    Use re for string type check

--- src/secrets_hunter/detectors/utils/entropy.py
+++ src/secrets_hunter/detectors/utils/entropy.py
@@ -1,13 +1,11 @@
 import math
-import string
+import re
 
 from collections import Counter
-from typing import Set
 
-HEX_CHARS: Set[str] = set(string.hexdigits)
-BASE64_CHARS: Set[str] = set(string.ascii_letters + string.digits) | {"+", "/", "="}
-BASE64URL_CHARS: Set[str] = BASE64_CHARS | {"-", "_"}
-
+HEX_RE = re.compile(r'^[0-9A-Fa-f]+$')
+BASE64_RE = re.compile(r'^[A-Za-z0-9+/=]+$')
+BASE64URL_RE = re.compile(r'^[A-Za-z0-9+/=_-]+$')
 
 def calculate_shannon_entropy(input_string: str) -> float:
     entropy = 0.0
@@ -27,10 +25,10 @@ def calculate_shannon_entropy(input_string: str) -> float:
     return entropy
 
 def is_hex_string(s: str) -> bool:
-    return bool(s) and all(c in HEX_CHARS for c in s)
+    return bool(s) and bool(HEX_RE.match(s))
 
 def is_base64_string(s: str) -> bool:
-    return bool(s) and all(c in BASE64_CHARS for c in s)
+    return bool(s) and bool(BASE64_RE.match(s))
 
 def is_base64url_string(s: str) -> bool:
-    return bool(s) and all(c in BASE64URL_CHARS for c in s)
+    return bool(s) and bool(BASE64URL_RE.match(s))
